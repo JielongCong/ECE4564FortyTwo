4ed3019d90bc271347d33c059d44b52eeb99251f
ltj623 <35505176+ltj623@users.noreply.github.com>
2019-10-27 18:22:28 -0400

    Delete ClientKeys.py

--- ClientKeys.py
+++
@@ -1,8 +0,0 @@
-#Twitter keys
-APIKey="qEMFJWaStvevFO5r2rEeXrnXQ"
-APISecretKey="RapQSRtA67OYFwf7CHpzVwAIQhaJ1kjqgFKeqI6KHyGQoeUsDI"
-Access="1176571953173475333-TY5iEpoCTjX6kvvk3KdNrvzcQmHu92"
-AccessSecret="mVGPbV9sQQajKSd93CMmF2xgtjoECXNJ191aEjJ8mTy8n"
-#IMB keys
-Iam_apikey="jtkm-0J8GqGX3t2grlxnmqOWjpQdu6lhNkedn1lUGxNl"
-Url="https://stream.watsonplatform.net/text-to-speech/api"
